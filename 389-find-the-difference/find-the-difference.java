af10166d29cf56e735cf7398abc82cbbab629df1
Jitendra Kumar <157132671+Jitendra-Kumar123@users.noreply.github.com>
2026-08-02 16:52:01 +0530

    Time: 3 ms (41.86%) | Memory: 43.1 MB (54.91%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,17 +1,38 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int [] freqArr = new int [26];
+        // int [] freqArr = new int [26];
+
+        // for(int i=0; i<s.length(); i++){
+        //     freqArr[s.charAt(i) - 'a']++;
+        // }
+
+        // for(int i=0; i<t.length(); i++){
+        //     freqArr[t.charAt(i) - 'a']--;
+        // }
+
+        // for(int i=0; i<26; i++){
+        //     if(freqArr[i] < 0 ){
+        //         return (char)(i + 'a');
+        //     }
+        // }
+
+        int [] sFreq = new int [26];
+        int [] tFreq = new int [26];
 
         for(int i=0; i<s.length(); i++){
-            freqArr[s.charAt(i) - 'a']++;
+            char ch = s.charAt(i);
+            int sIdx = ch - 'a';
+            sFreq[sIdx]++;
         }
 
         for(int i=0; i<t.length(); i++){
-            freqArr[t.charAt(i) - 'a']--;
+            char ch = t.charAt(i);
+            int tIdx = ch - 'a';
+            tFreq[tIdx]++;
         }
 
         for(int i=0; i<26; i++){
-            if(freqArr[i] < 0 ){
+            if(sFreq[i] != tFreq[i]){
                 return (char)(i + 'a');
             }
         }
