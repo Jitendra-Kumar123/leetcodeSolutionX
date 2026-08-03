e4a8b5f9e38fbef4102b639362ca4b5b5bb47ce1
Jitendra Kumar <157132671+Jitendra-Kumar123@users.noreply.github.com>
2026-08-03 18:37:13 +0530

    Time: 1 ms (99.97%) | Memory: 46.2 MB (89.95%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int n = ransomNote.length();
+        int m = magazine.length();
+
+        int [] freq = new int [26];
+
+        for(int i=0; i<n; i++){
+            freq[ransomNote.charAt(i) - 'a']++;
+        }
+
+        for(int i=0; i<m; i++){
+            freq[magazine.charAt(i) - 'a']--;
+        }
+
+        for(int i=0; i<26; i++){
+            if(freq[i] > 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
